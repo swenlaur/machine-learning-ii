4943a5023b60eb17ef2bc93e3fbd72cea2c26c3a
Sven Laur <swen@math.ut.ee>
2019-03-14 11:23:35 +0200

    Initial commit for practice session

---
+++ practise-sessions/03/data/readme.md
@@ -0,0 +1,3 @@
+Data files for the Markov model exercise:
+- random words in Estonian
+- random words in English
---
+++ practise-sessions/03/packages/hilbert_curve/examples.py
@@ -0,0 +1,22 @@
+import hilbert
+
+# we can go from distance along a curve to coordinates
+# in 2 dimensions with p=1 there are only 4 locations on the curve
+# [0, 0], [0, 1], [1, 1], [1, 0]
+p = 1
+N = 2
+hilbert_curve = hilbert.HilbertCurve(p, N)
+for ii in range(4):
+    print('coords(h={},p={},N={}) = {}'.format(
+        ii, p, N, hilbert_curve.coordinates_from_distance(ii)))
+
+
+# due to the magic of arbitrarily large integers in
+# Python (https://www.python.org/dev/peps/pep-0237/)
+# these calculations can be done with absurd numbers
+p = 512
+N = 10
+hilbert_curve = hilbert.HilbertCurve(p, N)
+ii = 123456789101112131415161718192021222324252627282930
+coords = hilbert_curve.coordinates_from_distance(ii)
+print('coords(h={},p={},N={}) = {}'.format(ii, p, N, coords))
